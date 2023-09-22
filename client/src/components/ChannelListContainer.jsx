a87120e7e1177e9759ed189081b458819fa6e772
=cs-308-2023 <yash7yash1@gmail.com>
2023-09-22 19:28:04 +0530

    done

---
+++ client/src/components/ChannelListContainer.jsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import { ChannelList, useChatContext } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+import { ChannelSearch, TeamChannel, TeamChannelPreview } from './';
+import AIConvosphereIcon from 'C:/Users/Rajendra/Desktop/IBY.inc/client/src/assets/chat.png'
+import LogoutIcon from 'C:/Users/Rajendra/Desktop/IBY.inc/client/src/assets/logout.png'
+
+
+import '../App.css';
+import TeamChannelList from './TeamChannelList';
+
+//eslint-disable
+const cookies = new Cookies();
+
+const SideBar = ({logout}) => (
+    <div className="channel-list__sidebar">
+        <div className="channel-list__sidebar__icon1">
+            <div className='icon1__inner'>
+                <img src={AIConvosphereIcon} alt="AIConvosphere" width="30" />
+
+            </div>
+
+        </div>
+        <div className="channel-list__sidebar__icon2">
+            <div className='icon1__inner' onClick={logout}>
+                <img src={LogoutIcon} alt="Logout" width="30" />
+
+            </div>
+
+        </div>
+
+    </div>
+);
+
+const CompanyHeader = () => (
+    <div className='channel-list__header'>
+        <p className='channel-list__header__text'>AIConvosphere</p>
+    </div>
+
+)
+const ChannelListContainer = () => {
+    const logout = () => {
+        cookies.remove("token");
+        cookies.remove('userId');
+        cookies.remove('username');
+        cookies.remove('fullName');
+        cookies.remove('avatarURL');
+        cookies.remove('hashedPassword');
+        cookies.remove('phoneNumber');
+
+        window.location.reload();
+
+    }
+    return (
+        <>
+            <SideBar logout  ={logout} />
+            <div className='channel-list__list__wrapper'>
+                <CompanyHeader />
+                <ChannelSearch />
+                <ChannelList
+                    filters={{}}
+                    channelRenderFilterFn={() => { }}
+                    List={(listProps) => (
+                        <TeamChannelList
+                            {...listProps}
+                            type="team"
+                        />
+                    )}
+                    Preview={(previewProps) => (
+                        <TeamChannelPreview
+                            {...previewProps}
+                            type=" team"
+
+                        />
+                    )}
+                />
+                <ChannelList
+                    filters={{}}
+                    channelRenderFilterFn={() => { }}
+                    List={(listProps) => (
+                        <TeamChannelList
+                            {...listProps}
+                            type="messaging"
+                        />
+                    )}
+                    Preview={(previewProps) => (
+                        <TeamChannelPreview
+                            {...previewProps}
+                            type=" messaging"
+
+                        />
+                    )}
+                />
+            </div>
+
+        </>
+
+    );
+}
+
+export default ChannelListContainer;
